ef1407a739d7d91f19b4c292ff477bd6cbc05bcd
Scott Todd <scott.todd0@gmail.com>
2014-10-05 16:54:13 -0400

    Started on basic camera movement and rotation.

---
+++ Game/Assets/ScottTest/Scripts/CameraControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject player;
+
+    // The camera moves around a sphere centered on the player.
+    // The radius is affected by the current zoom level.
+    // The two angles are controlled by mouse input.
+    public float radius = 10.0f;
+    public float verticalAngle = 20.0f; // X-Axis Euler Angle
+    public float horizontalAngle = 0.0f; // Y-Axis Euler Angle
+
+    void Awake() {
+        if (!player) { player = GameObject.FindWithTag("Player"); }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    void LateUpdate() {
+        // The default (no mouse buttons) behavior is to follow behind the player.
+        // Rotate the horizontalAngle towards the player's orientation.
+        // Maintain a constant verticalAngle.
+
+        Vector3 position = player.transform.position;
+        position -= player.transform.forward * radius;
+
+        horizontalAngle = -player.transform.eulerAngles.y - 90;
+
+        float phi = horizontalAngle * Mathf.Deg2Rad;
+        float theta = verticalAngle * Mathf.Deg2Rad;
+
+        float x = Mathf.Cos(theta) * Mathf.Cos(phi);
+        float y = Mathf.Sin(theta);
+        float z = Mathf.Cos(theta) * Mathf.Sin(phi);
+        Vector3 offset = new Vector3(x, y, z);
+        transform.position = player.transform.position + offset * radius;
+
+        transform.LookAt(player.transform.position);
+    }
+}
