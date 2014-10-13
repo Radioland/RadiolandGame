8f76ac51c2eedd0e5419ffd8aa386a05db5d2e1d
Xinoph <b.tam@live.com>
2014-10-13 13:21:07 -0400

    Added capacitor. Renamed texture for electrics. Some Gunther animation adjustments... (Generic)

---
+++ Game/Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,38 @@
+﻿//cameraFacingBillboard.cs v02
+//by Neil Carter (NCarter)
+//modified by Juan Castaneda (juanelo)
+//
+//added in-between GRP object to perform rotations on
+//added auto-find main camera
+//added un-initialized state, where script will do nothing
+using UnityEngine;
+using System.Collections;
+
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+	
+	public Camera m_Camera;
+	public bool amActive =false;
+	public bool autoInit =false;
+	GameObject myContainer;	
+	
+	void Awake(){
+		if (autoInit == true){
+			m_Camera = Camera.main;
+			amActive = true;
+		}
+		
+		myContainer = new GameObject();
+		myContainer.name = "GRP_"+transform.gameObject.name;
+		myContainer.transform.position = transform.position;
+		transform.parent = myContainer.transform;
+	}
+	
+	
+	void Update(){
+		if(amActive==true){
+			myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+		}
+	}
+}
