5e9b94f2024bb77f3981835a7d6e497e321f755b
Tim Terrezza <terret@rpi.edu>
2014-10-13 18:45:14 -0400

    added drops plus another jumping challenge

--- Game/Assets/Scripts/ObjectPickup.cs
+++ Game/Assets/Scripts/ObjectPickup.cs
@@ -2,61 +2,22 @@
 using System.Collections;
 
 public class ObjectPickup : MonoBehaviour {
-	GameObject wallManager;
-
-	float gravityTime;
-	bool lowGravity;
-
-	float jumpTime;
-	bool highJump;
+	GameObject objectiveGUI;
 	// Use this for initialization
 	void Start () {
-		wallManager = GameObject.Find("Hidden Wall Manager");
-
-		lowGravity = false;
-		gravityTime = Time.time;
-
-		highJump = false;
-		jumpTime = Time.time;
+		objectiveGUI = GameObject.Find ("ObjectiveUI");
 	}
 
 	void OnTriggerEnter (Collider c) {
 		if (c.gameObject.tag == "pickupable") {
 			Destroy(c.gameObject);
-			wallManager.GetComponent<HiddenWallManager>().IncrementTargets();
-		}
-		if (c.gameObject.tag == "antigravity") {
-			Destroy(c.gameObject);
-			lowGravity = true;
-			GetComponent<ThirdPersonController>().gravity = 10;
-			gravityTime = Time.time;
-		}
-		if (c.gameObject.tag == "highjump") {
-			Destroy(c.gameObject);
-			highJump = true;
-			GetComponent<ThirdPersonController>().jumpHeight = 2;
-			jumpTime = Time.time;
+			objectiveGUI.GetComponent<ObjectiveGUI>().IncrementTargets();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (lowGravity) {
-			GetComponent<GUIManager>().SetAntiGravity(true,5 - (Time.time - gravityTime));
-			if (Time.time - gravityTime > 5) {
-				lowGravity = false;
-				GetComponent<ThirdPersonController>().gravity = 20;
-				GetComponent<GUIManager>().SetAntiGravity(false,0);
-			}
-		}
-		if (highJump) {
-			GetComponent<GUIManager>().SetHighJump(true,5 - (Time.time - jumpTime));
-			if (Time.time - jumpTime > 5) {
-				highJump = false;
-				GetComponent<ThirdPersonController>().jumpHeight = 1;
-				GetComponent<GUIManager>().SetHighJump(false,0);
-			}
-		}
+
 	}
 	
 }
---
+++ Game/Assets/Scripts/ObjectiveGUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ObjectiveGUI : MonoBehaviour {
+	int targetsFound;
+	int totalTargets;
+	
+	Text text;
+	// Use this for initialization
+	void Start () {
+		targetsFound = 0;
+		totalTargets = 5;
+		text = GetComponent<Text>();
+	}
+	
+	void OnTriggerEnter (Collider c) {
+		if (c.gameObject.tag == "pickupable") {
+			Destroy(c.gameObject);
+			targetsFound += 1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = targetsFound.ToString() + "/" + totalTargets.ToString();
+	}
+	
+	public void SetTotalTargets(int targets) {
+		totalTargets = targets;
+	}
+	
+	public void IncrementTargets() {
+		targetsFound += 1;
+	}
+}
