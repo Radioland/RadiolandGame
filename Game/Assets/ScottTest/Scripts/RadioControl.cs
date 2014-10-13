c94f60eaec5695f601b9928dfb17e7e2aaf98133
Scott Todd <scott.todd0@gmail.com>
2014-10-13 13:58:12 -0400

    Added initial radio UI with station tuning.

---
+++ Game/Assets/ScottTest/Scripts/RadioControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+//using System.Collections.Generic;
+
+public class RadioControl : MonoBehaviour
+{
+    public float frequencyFadeLimit = 0.1f;
+    public float currentFrequency { get { return radioDialSlider.value; } }
+
+    // Private variables set in the inspector.
+    [SerializeField] private Slider radioDialSlider;
+
+    private RadioStation[] stations;
+
+    void Awake() {
+        if (!radioDialSlider) {
+            Debug.LogWarning("Please set the Slider for RadioControl.");
+            gameObject.SetActive(false);
+        }
+
+        stations = gameObject.GetComponentsInChildren<RadioStation>();
+        foreach (RadioStation station in stations) {
+            station.radioControl = this;
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Alpha1)) {
+            radioDialSlider.value -= 0.01f;
+        }
+        if (Input.GetKey(KeyCode.Alpha2)) {
+            radioDialSlider.value += 0.01f;
+        }
+
+        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
+        radioDialSlider.value += scrollValue;
+    }
+}
---
+++ Game/Assets/ScottTest/Scripts/RadioStation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class RadioStation : MonoBehaviour
+{
+    public float frequency;
+
+    [HideInInspector] public RadioControl radioControl;
+    [HideInInspector] public AudioSource audioSource;
+
+    void Awake() {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.volume = 0;
+    }
+
+    void Start() {
+        if (!radioControl) {
+            Debug.LogWarning("There is no RadioControl linked to this RadioStation!");
+        }
+    }
+
+    void Update() {
+        // Lerp signalStrength from 0 to 1 within frequencyFadeLimit.
+        // When outside of frequencyFadeLimit from the current frequency, clamp to 0.
+        float frequencyDifference = Mathf.Abs(radioControl.currentFrequency - frequency);
+        float signalStrength = 1.0f - frequencyDifference / radioControl.frequencyFadeLimit;
+        signalStrength = Mathf.Clamp(signalStrength, 0.0f, 1.0f);
+
+        audioSource.volume = signalStrength;
+    }
+}
