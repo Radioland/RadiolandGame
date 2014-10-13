70eb6b3f334e93c2332b0d56dfc499cd41c5b00b
Xinoph <b.tam@live.com>
2014-10-13 19:52:11 -0400

    Adjusted walk/run transitions for Gunther. Added initial dynamics to hair.

---
+++ Game/Assets/Scripts/UnparentToWorld.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnparentToWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = null;
+	}
+
+}
