378b4fcd111f2bc2c300bd969613cb3a7431cc6b
Xinoph <b.tam@live.com>
2014-10-13 20:47:20 -0400

    Resized resistor & capacitor. Added capacitor effects.

---
+++ Game/Assets/Scripts/AnimateUVs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateUVs : MonoBehaviour {
+	public float scrollSpeed = 0.5F;
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+	}
+}
--- Game/Assets/Scripts/CameraFacingBillboard.cs
+++ Game/Assets/Scripts/CameraFacingBillboard.cs
@@ -1,38 +1,19 @@
-﻿//cameraFacingBillboard.cs v02
-//by Neil Carter (NCarter)
-//modified by Juan Castaneda (juanelo)
-//
-//added in-between GRP object to perform rotations on
-//added auto-find main camera
-//added un-initialized state, where script will do nothing
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
-
 
 public class CameraFacingBillboard : MonoBehaviour
 {
+	public GameObject target;
 	
-	public Camera m_Camera;
-	public bool amActive =false;
-	public bool autoInit =false;
-	GameObject myContainer;	
-	
-	void Awake(){
-		if (autoInit == true){
-			m_Camera = Camera.main;
-			amActive = true;
-		}
-		
-		myContainer = new GameObject();
-		myContainer.name = "GRP_"+transform.gameObject.name;
-		myContainer.transform.position = transform.position;
-		transform.parent = myContainer.transform;
+	void  Awake ()
+	{
+		// if no object referenced, grab the main camera
+		if (!target)
+			target = Camera.main.gameObject; 
 	}
 	
-	
-	void Update(){
-		if(amActive==true){
-			myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
-		}
+	void  Update ()
+	{
+		transform.LookAt (target.transform);
 	}
 }
