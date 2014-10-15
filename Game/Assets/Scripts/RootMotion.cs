d2bcba77521e5816e638ffa25813e2c98d63a46f
Xinoph <b.tam@live.com>
2014-10-14 21:50:24 -0400

    Added strafe animation / motion to Gunther. Added birb model & animation.

---
+++ Game/Assets/Scripts/RootMotion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class RootMotion : MonoBehaviour {
+	
+	void OnAnimatorMove()
+	{
+		Animator animator = GetComponent<Animator>(); 
+		
+		if (animator)
+		{
+			Vector3 newPosition = transform.position;
+			newPosition.x += animator.GetFloat("Runspeed") * Time.deltaTime; 
+			transform.position = newPosition;
+		}
+	}
+}
