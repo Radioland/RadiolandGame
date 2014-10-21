038e03a98bde050b14568ae06e4960798dbbebd7
Scott Todd <scott.todd0@gmail.com>
2014-10-21 18:49:39 -0400

    Added wobble and rotate scripts with demos in islands_test scene.

---
+++ Game/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    [Tooltip("Euler angle degrees per second.")]
+    [SerializeField] private Vector3 angularSpeed;
+
+    void Awake() {
+
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+        transform.Rotate(angularSpeed * Time.deltaTime);
+    }
+}
