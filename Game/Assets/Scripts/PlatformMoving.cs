64cc86bb20ab4f70977fad6e94bf4c96c132c427
Tim Terrezza <terret@rpi.edu>
2014-10-22 15:54:53 -0400

    first Segment done

--- Game/Assets/Scripts/PlatformMoving.cs
+++ Game/Assets/Scripts/PlatformMoving.cs
@@ -5,22 +5,24 @@ public class PlatformMoving : MonoBehaviour {
 
     private float startTime;
     public Vector3 velocity;
-	private Vector3 topVelocity;
+    private Vector3 topVelocity;
+
+    public float movementTime;
+    public float pauseTime;
 
     void Start () {
-        startTime = Time.time;
-		topVelocity = velocity;
+        topVelocity = velocity;
     }
     
-    void FixedUpdate () {
-        if (Time.time - startTime > 2) {
-			velocity = new Vector3(0,0,0);
+    void Update () {
+        if (Time.time - startTime > movementTime) {
+            velocity = new Vector3(0,0,0);
         }
-		if (Time.time - startTime > 3) {
-			velocity = -1 * topVelocity;
-			topVelocity = velocity;
-			startTime = Time.time;
-		}
+        if (Time.time - startTime > pauseTime + movementTime) {
+            velocity = -1 * topVelocity;
+            topVelocity = velocity;
+            startTime = Time.time;
+        }
 
         transform.localPosition += velocity * Time.deltaTime;
     
