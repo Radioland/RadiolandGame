1eacf6ddc23b924f8d7b4457d2733999d8dd9997
Scott Todd <scott.todd0@gmail.com>
2014-10-28 16:46:20 -0400

    Added checkpoints and killzones, demonstrated in islands_test scene.

---
+++ Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+
+    private GameObject latestCheckpoint;
+
+    void Awake() {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void SetLatestCheckpoint(GameObject checkpoint) {
+        latestCheckpoint = checkpoint;
+    }
+
+    public void KillPlayer() {
+        // TODO: effects, penalty, respawn animation, etc.
+
+        Region latestCheckpointRegion = latestCheckpoint.GetComponent<Region>();
+        player.transform.position = latestCheckpointRegion.GetCenter();
+    }
+}
---
+++ Game/Assets/Scripts/Killzone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killzone : MonoBehaviour
+{
+    private GameController gameController;
+
+    void Awake() {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        gameController = gameControllerObject.GetComponent<GameController>();
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
+            gameController.KillPlayer();
+        }
+    }
+}
