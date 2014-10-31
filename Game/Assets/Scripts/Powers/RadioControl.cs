f9f54792e9584097448d651b07e33677aa177515
Scott Todd <scott.todd0@gmail.com>
2014-10-31 00:03:13 -0400

    Imported new sounds from Matt. Fill in between stations with static.
    Static fades in when tuning and fades out after a period of inactivity.
    Static volume is inversely proportional to the volume of the closest station.
    Also tuned frequency limits and station frequencies so powerups are easier to use.

--- Game/Assets/Scripts/Powers/RadioControl.cs
+++ Game/Assets/Scripts/Powers/RadioControl.cs
@@ -4,8 +4,8 @@ using System.Collections;
 
 public class RadioControl : MonoBehaviour
 {
-    public float frequencyFadeLimit = 0.1f;
-    public float powerupMinSignalStrength = 0.5f;
+    public float frequencyFadeLimit = 0.2f;
+    public float powerupMinSignalStrength = 0.25f;
     public float currentFrequency { get { return radioDialSlider.value; } }
 
     // Private variables set in the inspector.
@@ -14,8 +14,21 @@ public class RadioControl : MonoBehaviour
     [SerializeField] private Image energyGlowImage;
     [SerializeField] private RectTransform radioKnobTransform;
     [SerializeField] private float knobTurnRatio = 4.0f;
+    [Tooltip("Plays when seeking between stations.")]
+    [SerializeField] private AudioSource staticSource;
+    [SerializeField] [Range(0.0f, 1.0f)] private float staticMaxVolume = 0.5f;
+    [SerializeField] [Range(0.0f, 1.0f)] private float staticCutoff = 0.2f;
+    [SerializeField] private float staticFadeTime = 3.0f;
+    [SerializeField] private float staticLingerTime = 1.0f;
 
     private RadioStation[] stations;
+    private bool inUse;
+    private float lastStartedTime;
+    private float lastActiveTime;
+    private float lastDecreaseVolume;
+    private float lastIncreaseVolume;
+
+    public float volume;
 
     void Awake() {
         if (!powerupManager) {
@@ -40,14 +53,30 @@ public class RadioControl : MonoBehaviour
             }
         }
 
+        if (!staticSource) {
+            Debug.LogWarning("[Audio] Please set the staticSource for RadioControl.");
+        } else {
+            staticSource.volume = 0.0f;
+        }
+
         stations = gameObject.GetComponentsInChildren<RadioStation>();
         foreach (RadioStation station in stations) {
             station.radioControl = this;
         }
+
+        ResetStatic();
     }
 
     void Start() {
 
+    }
+
+    void ResetStatic() {
+        inUse = false;
+        lastStartedTime = -1000.0f;
+        lastActiveTime = -1000.0f;
+        lastIncreaseVolume = 0.0f;
+        lastDecreaseVolume = 0.0f;
     }
 
     void Update() {
@@ -62,6 +91,21 @@ public class RadioControl : MonoBehaviour
         float scrollValue = Input.GetAxis("Mouse ScrollWheel");
         radioDialSlider.value -= scrollValue;
 
+        // Track activity using raw input.
+        float rawScroll = Input.GetAxisRaw("Mouse ScrollWheel");
+        if (Mathf.Abs(rawScroll) > 0.001f) {
+            if (!inUse) {
+                inUse = true;
+                if (Time.time - lastActiveTime > staticLingerTime) {
+                    lastStartedTime = Time.time;
+                }
+            }
+            lastActiveTime = Time.time;
+        } else {
+            inUse = false;
+        }
+
+        // Rotate the knob by a factor of the value.
         if (radioKnobTransform) {
             float rotationDegrees = radioDialSlider.value * 360.0f * knobTurnRatio;
             radioKnobTransform.localRotation = Quaternion.Euler(0, 0, rotationDegrees);
@@ -74,12 +118,41 @@ public class RadioControl : MonoBehaviour
             energyGlowImage.color = newColor;
         }
 
+        // Trigger powerups.
         if (Input.GetButtonDown("Fire1")) {
             foreach (RadioStation station in stations) {
                 if (station.signalStrength > powerupMinSignalStrength) {
                     station.UsePowerup();
                 }
             }
+        }
+
+        // Adjust the volume of the static to fill in between stations.
+        if (staticSource) {
+            float maxSignal = 0.0f;
+            foreach (RadioStation station in stations) {
+                maxSignal = Mathf.Max(maxSignal, station.signalStrength);
+            }
+
+            float staticStrength = (1.0f - maxSignal / staticCutoff) * staticMaxVolume;
+
+            if (inUse || Time.time - lastActiveTime < staticLingerTime) {
+                // Fade in.
+                float tStart = (Time.time - lastStartedTime) / staticFadeTime;
+                volume = Mathf.Lerp(lastDecreaseVolume, staticStrength, Mathf.Clamp01(tStart));
+                lastIncreaseVolume = volume;
+            } else {
+                // Fade out.
+                float tEnd = (Time.time - staticLingerTime - lastActiveTime) / staticFadeTime;
+                volume = Mathf.Lerp(lastIncreaseVolume, 0.0f, Mathf.Clamp01(tEnd));
+                lastDecreaseVolume = volume;
+            }
+
+            if (maxSignal > staticCutoff) {
+                ResetStatic();
+            }
+
+            staticSource.volume = volume;
         }
     }
 }
