5a7637d6489ed63a8956a2a052f6dd164822cf8b
Scott Todd <scott.todd0@gmail.com>
2014-11-01 14:39:50 -0400

    Scale landing dust particle speed and size with landing speed.

--- Game/Assets/Scripts/Landing.cs
+++ Game/Assets/Scripts/Landing.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class Landing : MonoBehaviour
 {
     [SerializeField] private EffectManager landingEffects;
+    [SerializeField] private ParticleSystem landingParticles;
+    [Tooltip("Minimum landing speed to trigger effects, affects interpolation.")]
+    [SerializeField] private float minEffectSpeed = 12.0f;
+    [Tooltip("Maximum landing speed for interpolation purposes, effects still trigger past this.")]
+    [SerializeField] private float maxEffectSpeed = 30.0f;
+    [SerializeField] private float minStartSize = 0.05f;
+    [SerializeField] private float maxStartSize = 2.0f;
+    [SerializeField] private float minStartSpeed = 0.2f;
+    [SerializeField] private float maxStartSpeed = 3.0f;
 
     void Awake() {
 
@@ -19,7 +28,12 @@ public class Landing : MonoBehaviour
     
     // Called via SendMessage in CharacterMovement.
     private void Grounded(float verticalSpeed) {
-        if (landingEffects) {
+        if (landingEffects && -verticalSpeed > minEffectSpeed) {
+            float t = Mathf.InverseLerp(minEffectSpeed, maxEffectSpeed, -verticalSpeed);
+
+            landingParticles.startSize = Mathf.Lerp(minStartSize, maxStartSize, t);
+            landingParticles.startSpeed = Mathf.Lerp(minStartSpeed, maxStartSpeed, t);
+
             landingEffects.StartEvent();
         }
     }
