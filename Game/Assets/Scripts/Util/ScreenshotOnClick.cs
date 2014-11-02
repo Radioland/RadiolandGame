18b47d54947a62f929366bdd62a452e9880aacd6
Xinoph <b.tam@live.com>
2014-11-02 15:54:36 -0500

    Added DIP dance. Added screenshot script / scene.

---
+++ Game/Assets/Scripts/Util/ScreenshotOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenshotOnClick : MonoBehaviour {
+
+	public int superSize = 1;
+	
+	private bool takeHiResShot = false;
+	
+	void LateUpdate() {
+		takeHiResShot |= Input.GetKeyDown("k");
+		if (takeHiResShot) 
+		{
+			Application.CaptureScreenshot("Screenshot.png", superSize);
+			Debug.Log("Took screenshot");
+			takeHiResShot = false;
+		}
+	}
+}
