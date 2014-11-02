dfc7b34e5838682e4ba6770d1697564e3a4a3864
Xinoph <b.tam@live.com>
2014-11-02 11:14:00 -0500

    Added light crystal prefab and animated it. Linked light crystal's light color to rest of its renderers. Moved some utility scripts into appropriate folder.

---
+++ Game/Assets/Scripts/Util/LinkRendererToLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkRendererToLight : MonoBehaviour {
+	
+	public Light theLight;
+	public string shaderColorName = "_Color";
+
+	void Awake () {
+		if (!renderer.material.HasProperty(shaderColorName)) {
+			Debug.LogWarning(renderer.GetPath() + " has no material color named " + shaderColorName);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (renderer.material.HasProperty(shaderColorName)) {
+			if (renderer.material.GetColor(shaderColorName) != theLight.color) {
+				renderer.material.SetColor(shaderColorName, theLight.color);
+			}
+		}
+	}
+}
