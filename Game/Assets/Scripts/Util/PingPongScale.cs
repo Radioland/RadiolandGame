0d84ef139600f03ec8034ccc06e631d3289b5e34
Scott Todd <scott.todd0@gmail.com>
2014-11-02 11:38:08 -0500

    Added PingPongScale script.

---
+++ Game/Assets/Scripts/Util/PingPongScale.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongScale : MonoBehaviour
+{
+    [SerializeField] private float minScale = 1.0f;
+    [SerializeField] private float maxScale = 1.0f;
+    [SerializeField] private float speed = 1.0f;
+
+    void Awake() {
+
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+        float pingPong = Mathf.PingPong(Time.time * speed, 1.0f);
+        float scaleFactor = Mathf.Lerp(minScale, maxScale, pingPong);
+        transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+    }
+}
