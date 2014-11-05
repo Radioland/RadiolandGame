bfa16c02e117beddca27aa8531b736a4f94413e0
Scott Todd <scott.todd0@gmail.com>
2014-11-05 09:23:36 -0500

    Added a minimum speed required to bounce.

--- Game/Assets/Scripts/Bounce.cs
+++ Game/Assets/Scripts/Bounce.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Bounce : MonoBehaviour
 {
+    [SerializeField] private float minimumSpeed = 10.0f;
+
     private CharacterMovement characterMovement;
 
     void Awake() {
@@ -23,7 +25,7 @@ public class Bounce : MonoBehaviour
 
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.2f)) {
             BouncePlatform bouncePlatform = hit.collider.gameObject.GetComponent<BouncePlatform>();
-            if (bouncePlatform) {
+            if (bouncePlatform && -verticalSpeed > minimumSpeed) {
                 characterMovement.Bounce(-verticalSpeed * bouncePlatform.elasticity);
             }
         }
