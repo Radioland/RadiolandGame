7558900e3959224d7808ca62ea2b84cadaeb0e30
Xinoph <b.tam@live.com>
2014-11-05 10:35:33 -0500

    Changed lighting. Fixed floating critters.

---
+++ Game/Assets/Scripts/Util/ParentToObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParentToObject : MonoBehaviour {
+
+	public GameObject Parent;
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = Parent.transform;
+	}
+}
