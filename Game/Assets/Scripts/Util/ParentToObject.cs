ec019ff41f6c69b922ab7dd20013dcd05ca26f55
Xinoph <b.tam@live.com>
2014-11-09 15:09:10 -0500

    Adjusted meta files for Annie's islands. Added snow prefab. Added waterfall island model / prefab.

--- Game/Assets/Scripts/Util/ParentToObject.cs
+++ Game/Assets/Scripts/Util/ParentToObject.cs
@@ -5,8 +5,14 @@ public class ParentToObject : MonoBehaviour {
 
 	public GameObject Parent;
 
+	public bool hasOffset;
+	public Vector3 offset;
+
 	// Use this for initialization
 	void Start () {
 		transform.parent = Parent.transform;
+		if (hasOffset) {
+			transform.position = transform.parent.transform.position + offset;
+		}
 	}
 }
