200949bb8d98de65362d6effc00a05c5b88f0b08
Scott Todd <scott.todd0@gmail.com>
2014-11-11 23:59:40 -0500

    Initial test of wind push areas.

---
+++ Game/Assets/Scripts/PushedByWind.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushedByWind : MonoBehaviour
+{
+    private CharacterController controller;
+    
+    void Awake() {
+        controller = gameObject.GetComponent<CharacterController>();
+        
+        if (!controller) {
+            Debug.LogWarning(transform.GetPath() + " does not have a CharacterController.");
+            this.enabled = false;
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+    
+    void OnTriggerStay(Collider other) {
+        WindPushArea windPushArea = other.gameObject.GetComponent<WindPushArea>();
+        if (windPushArea) {
+            controller.Move(windPushArea.windVelocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Game/Assets/Scripts/WindPushArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindPushArea : MonoBehaviour
+{
+    public Vector3 windVelocity;
+
+    void Awake() {
+
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
