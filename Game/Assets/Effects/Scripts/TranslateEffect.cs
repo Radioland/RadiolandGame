7a0f6ab8b3d5853a0e1af971322a457e101b9bda
Scott Todd <scott.todd0@gmail.com>
2014-11-15 20:50:33 -0500

    Animate phone and cassette pickup manually (animation was glitchy?).

---
+++ Game/Assets/Effects/Scripts/TranslateEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslateEffect : Effect
+{
+    [SerializeField] private Vector3 finalTranslationDelta;
+
+    protected override void Awake() {
+        base.Awake();
+    }
+    
+    protected override void Start() {
+        base.Start();
+    }
+    
+    protected override void Update() {
+        base.Update();
+
+        if (hasStarted) {
+            transform.position = transform.position + finalTranslationDelta / duration * Time.deltaTime;
+        }
+    }
+    
+    public override void TriggerEffect() {
+        base.TriggerEffect();
+    }
+    
+    public override void StartEffect() {
+        base.StartEffect();
+    }
+    
+    public override void EndEffect() {
+        base.EndEffect();
+    }
+}
