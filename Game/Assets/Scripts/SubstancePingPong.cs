6d67431d9678d50c5f4cafe7a5923d72d9192864
Xinoph <b.tam@live.com>
2014-11-17 11:14:39 -0500

    Adjusted shadows in levels. Added temporary tree for testing...

---
+++ Game/Assets/Scripts/SubstancePingPong.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubstancePingPong : MonoBehaviour {
+	public string floatRangeProperty = "Disorder";
+	public float scale = 100;
+	public float cycleTime = 10;
+	void Update() {
+		ProceduralMaterial substance = renderer.sharedMaterial as ProceduralMaterial;
+		if (substance) {
+			float lerp = Mathf.PingPong(Time.time * 2 / cycleTime, 1) * scale;
+			substance.SetProceduralFloat(floatRangeProperty, lerp);
+			substance.RebuildTextures();
+		}
+	}
+}
