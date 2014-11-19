e104c7be1774743bca8f6eae40b01ac0d732438a
Tim Terrezza <terret@rpi.edu>
2014-11-19 17:07:10 -0500

    inital work on start menus and continue and stuff

---
+++ Game/Assets/Scripts/Menus/PlayerLevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLevelManager : MonoBehaviour {
+
+	void Awake() {
+		PlayerPrefs.SetInt("level", Application.loadedLevel);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Game/Assets/Scripts/Menus/StartMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+	[SerializeField] private GameObject continueButton;
+	[SerializeField] private GameObject newGameButton;
+	[SerializeField] private GameObject exitButton;
+	[SerializeField] private GameObject initialText;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetButtonDown(buttonName:"Submit")) {
+			continueButton.SetActive(true);
+			continueButton.GetComponent<Button>();
+			newGameButton.SetActive(true);
+			exitButton.SetActive(true);
+			initialText.SetActive(false);
+		}
+	}
+
+	public void GoContinue() {
+		if (PlayerPrefs.GetInt("level") != 0) {
+			Application.LoadLevel(PlayerPrefs.GetInt("level"));
+		}
+		else {
+			Application.LoadLevel(1);
+		}
+	}
+
+	public void GoNewGame() {
+		Application.LoadLevel(1);
+	}
+
+	public void GoExit() {
+		Application.Quit();
+	}
+}
