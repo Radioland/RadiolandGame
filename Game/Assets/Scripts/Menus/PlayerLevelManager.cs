831a76c497b609917e1712f8be7e14fc1f2ce156
Tim Terrezza <terret@rpi.edu>
2014-11-19 17:30:31 -0500

    press p to pause mid game

--- Game/Assets/Scripts/Menus/PlayerLevelManager.cs
+++ Game/Assets/Scripts/Menus/PlayerLevelManager.cs
@@ -3,11 +3,23 @@ using System.Collections;
 
 public class PlayerLevelManager : MonoBehaviour {
 
+	bool paused = false;
+
 	void Awake() {
 		PlayerPrefs.SetInt("level", Application.loadedLevel);
 	}
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.P)) {
+			if (paused) {
+				paused = false;
+				Time.timeScale = 1;
+			}
+			else {
+				paused = true;
+				//Timescale is not 0 because it throws an error with FollowPlatforms.cs line 58
+				Time.timeScale = 0.0000000001f;
+			}
+		}
 	}
 }
