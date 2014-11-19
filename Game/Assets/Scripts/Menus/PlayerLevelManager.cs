fd5adc5e7360e074c0c2eba867ebecbe77580f62
Tim Terrezza <terret@rpi.edu>
2014-11-19 17:50:24 -0500

    added blacking out effect for pause

--- Game/Assets/Scripts/Menus/PlayerLevelManager.cs
+++ Game/Assets/Scripts/Menus/PlayerLevelManager.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class PlayerLevelManager : MonoBehaviour {
 
-	bool paused = false;
+	private bool paused = false;
+
+	[SerializeField] private GameObject background;
 
 	void Awake() {
 		PlayerPrefs.SetInt("level", Application.loadedLevel);
@@ -14,11 +16,13 @@ public class PlayerLevelManager : MonoBehaviour {
 			if (paused) {
 				paused = false;
 				Time.timeScale = 1;
+				background.SetActive(false);
 			}
 			else {
 				paused = true;
 				//Timescale is not 0 because it throws an error with FollowPlatforms.cs line 58
 				Time.timeScale = 0.0000000001f;
+				background.SetActive(true);
 			}
 		}
 	}
