c25b2063b6d3960d616954c123b09c7ef2ff81e2
Scott Todd <scott.todd0@gmail.com>
2014-11-22 22:10:59 -0500

    Limit bouncing to magnifying only the initial landing speed.

--- Game/Assets/Scripts/Bounce.cs
+++ Game/Assets/Scripts/Bounce.cs
@@ -6,9 +6,13 @@ public class Bounce : MonoBehaviour
     [SerializeField] private float minimumSpeed = 10.0f;
 
     private CharacterMovement characterMovement;
+    private float firstBounceSpeed;
+    private bool bouncing;
 
     void Awake() {
         characterMovement = gameObject.GetComponent<CharacterMovement>();
+
+        bouncing = false;
     }
 
     void Start() {
@@ -25,9 +29,24 @@ public class Bounce : MonoBehaviour
 
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.2f)) {
             BouncePlatform bouncePlatform = hit.collider.gameObject.GetComponent<BouncePlatform>();
-            if (bouncePlatform && -verticalSpeed > minimumSpeed) {
-                characterMovement.Bounce(-verticalSpeed * bouncePlatform.elasticity);
+            if (bouncePlatform) {
+                ApplyBounce(verticalSpeed, bouncePlatform);
+            } else {
+                bouncing = false;
             }
+        }
+    }
+
+    private void ApplyBounce(float verticalSpeed, BouncePlatform bouncePlatform) {
+        if (!bouncing) {
+            firstBounceSpeed = verticalSpeed;
+        }
+
+        if (-firstBounceSpeed > minimumSpeed) {
+            characterMovement.Bounce(-firstBounceSpeed * bouncePlatform.elasticity);
+            bouncing = true;
+        } else {
+            bouncing = false;
         }
     }
 }
