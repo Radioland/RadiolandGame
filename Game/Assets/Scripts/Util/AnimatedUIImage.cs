38ce703df7c9ab84d18e99c703d23bc9db9a0fa2
Xinoph <b.tam@live.com>
2014-11-23 11:46:10 -0500

    Initial pass of making UI clearer...

---
+++ Game/Assets/Scripts/Util/AnimatedUIImage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimatedUIImage : MonoBehaviour {
+
+	public int FPS;
+	public List<Sprite> Sprites = new List<Sprite>();
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
