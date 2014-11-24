424a2ee32f972d27ea1d09d35f93f3cdc16b25ac
Tim Terrezza <terret@rpi.edu>
2014-11-24 16:52:21 -0500

    initial tutorial work

---
+++ Game/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialTrigger : MonoBehaviour {
+
+	[SerializeField] private string messageText;
+	[SerializeField] private GameObject textObject;
+
+	private bool visited;
+
+	void Start () {
+		visited = false;
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "Player") {
+			if (visited == false) {
+				textObject.GetComponent<Text>().text = messageText;
+				visited = true;
+			}
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
