28efc8394eedbeadc33a62e91ba8114f6acdbfb6
Tim Terrezza <terret@rpi.edu>
2014-12-01 12:05:35 -0500

    Gameover scene, start menu change

---
+++ Game/Assets/Scripts/Menus/Gameover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gameover : MonoBehaviour {
+
+	[SerializeField] private GameObject newGameButton;
+	[SerializeField] private GameObject exitButton;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+	}
+	
+	public void GoNewGame() {
+		Application.LoadLevel(1);
+	}
+	
+	public void GoExit() {
+		Application.Quit();
+	}
+}
