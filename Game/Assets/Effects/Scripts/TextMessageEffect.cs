d6fe2e3aa6de7e21c7d3675a68f74986ca6616cf
Scott Todd <scott.todd0@gmail.com>
2014-12-02 23:29:38 -0500

    Replaced TutorialTrigger with TextMessageEffect

---
+++ Game/Assets/Effects/Scripts/TextMessageEffect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextMessageEffect : Effect
+{
+    // Variables to specify in the editor.
+    [SerializeField] private string message;
+    [SerializeField] private Text text;
+    
+    protected override void Awake() {
+        base.Awake();
+    }
+    
+    protected override void Start() {
+        base.Start();
+    }
+    
+    protected override void Update() {
+        base.Update();
+    }
+    
+    public override void TriggerEffect() {
+        base.TriggerEffect();
+    }
+    
+    public override void StartEffect() {
+        base.StartEffect();
+        
+        if (text) {
+            text.text = message;
+        }
+    }
+    
+    public override void EndEffect() {
+        base.EndEffect();
+    }
+}
