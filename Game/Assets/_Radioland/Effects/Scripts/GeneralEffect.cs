4f0c3d6ce9f920bd53bb0ee2c19dc8e7e9b55bde
Scott Todd <scott.todd0@gmail.com>
2015-01-12 00:33:07 -0500

    Added GeneralEffect using UnityEvents, with a forced jumping zone demo.

---
+++ Game/Assets/_Radioland/Effects/Scripts/GeneralEffect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class GeneralEffect : Effect
+{
+    [SerializeField] private UnityEvent triggerEvent;
+    [SerializeField] private UnityEvent startEvent;
+    [SerializeField] private UnityEvent endEvent;
+
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    protected override void Start() {
+        base.Start();
+    }
+
+    protected override void Update() {
+        base.Update();
+    }
+
+    public override void TriggerEffect() {
+        base.TriggerEffect();
+
+        triggerEvent.Invoke();
+    }
+
+    public override void StartEffect() {
+        base.StartEffect();
+
+        startEvent.Invoke();
+    }
+
+    public override void EndEffect() {
+        base.EndEffect();
+
+        endEvent.Invoke();
+    }
+}
