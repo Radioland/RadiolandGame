176f51eb6e6eb8c49fbbb281498d9a144b650e3c
Scott Todd <scott.todd0@gmail.com>
2015-02-22 14:47:12 -0500

    Draw gizmo wireframe boxes at the target transform.

--- Game/Assets/_Radioland/Scripts/Util/TransformLerp.cs
+++ Game/Assets/_Radioland/Scripts/Util/TransformLerp.cs
@@ -14,12 +14,11 @@ public class TransformLerp : MonoBehaviour
     private float currentTime;
     private float timeDamp;
 
-    private void Awake() {
-        initialPosition = transform.position;
-        targetPosition = initialPosition + finalTranslationDelta;
+    private static readonly Color gizmoBoxColor = Color.grey;
+    private static readonly Color selectedGizmoBoxColor = Color.blue;
 
-        initialRotation = transform.localRotation;
-        targetRotation = Quaternion.Euler(finalLocalRotation);
+    private void Awake() {
+        SetupTransformations();
 
         currentTime = 0f;
         timeDamp = 0f;
@@ -27,6 +26,14 @@ public class TransformLerp : MonoBehaviour
 
     private void Start() {
 
+    }
+
+    private void SetupTransformations() {
+        initialPosition = transform.position;
+        targetPosition = initialPosition + finalTranslationDelta;
+
+        initialRotation = transform.localRotation;
+        targetRotation = Quaternion.Euler(finalLocalRotation);
     }
 
     private void Update() {
@@ -42,5 +49,33 @@ public class TransformLerp : MonoBehaviour
 
     public void SetTimeSmooth(float t) {
         SetTime(Mathf.SmoothDamp(currentTime, t, ref timeDamp, duration));
+    }
+
+    private void DrawGizmos() {
+        if (!Application.isPlaying) {
+            SetupTransformations();
+        }
+
+        Quaternion referenceRotation = transform.parent ?
+                                           transform.parent.rotation * targetRotation :
+                                           targetRotation;
+        Matrix4x4 gizmoMatrix = Matrix4x4.TRS(targetPosition, referenceRotation, Vector3.one);
+        Gizmos.matrix = gizmoMatrix;
+
+        if (renderer) {
+            Gizmos.DrawWireCube(Vector3.zero, renderer.bounds.extents);
+        } else if (collider) {
+            Gizmos.DrawWireCube(Vector3.zero, collider.bounds.extents);
+        }
+    }
+
+    public void OnDrawGizmos() {
+        Gizmos.color = gizmoBoxColor;
+        DrawGizmos();
+    }
+
+    public void OnDrawGizmosSelected() {
+        Gizmos.color = selectedGizmoBoxColor;
+        DrawGizmos();
     }
 }
