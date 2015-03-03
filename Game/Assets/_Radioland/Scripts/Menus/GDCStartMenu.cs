41e3dad19023dbf9462d6a4bcd95bd5cd65772bc
Scott Todd <scott.todd0@gmail.com>
2015-03-02 17:48:50 -0800

    Added GDC Start menu scene and updated build settings.

---
+++ Game/Assets/_Radioland/Scripts/Menus/GDCStartMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GDCStartMenu : MonoBehaviour
+{
+    private void Awake() {
+
+    }
+    private void Start() {
+
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Exit();
+        }
+    }
+
+    public void LoadLevel(int level) {
+        Application.LoadLevel(level);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
