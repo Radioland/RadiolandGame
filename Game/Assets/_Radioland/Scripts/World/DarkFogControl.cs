37bf1f01120c2f7bb8f0775409ce590566fc12fb
Scott Todd <scott.todd0@gmail.com>
2015-03-31 17:07:43 -0400

    Store fog level, increase on collision with zones, display on UI.

---
+++ Game/Assets/_Radioland/Scripts/World/DarkFogControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DarkFogControl : MonoBehaviour
+{
+    public float fogLevel = 0f;
+
+    [SerializeField] private Slider displaySlider;
+
+    private float fogLevelTarget;
+    private float fogSmoothVel = 0f;
+    private const float fogSmoothTime = 0.5f;
+
+    private void Awake() {
+        if (displaySlider) { displaySlider.value = fogLevel; }
+
+        Messenger.AddListener<float>("ChangeFogLevel", OnChangeFogLevel);
+        Messenger.AddListener<float>("ChangeFogLevelSmooth", OnChangeFogLevelSmooth);
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        fogLevel = Mathf.SmoothDamp(fogLevel, fogLevelTarget, ref fogSmoothVel, fogSmoothTime);
+
+        if (displaySlider) { displaySlider.value = fogLevel; }
+    }
+
+    private void OnChangeFogLevel(float change) {
+        fogLevel += change;
+        fogLevelTarget = fogLevel;
+    }
+
+    private void OnChangeFogLevelSmooth(float change) {
+        fogLevelTarget += change;
+    }
+}
