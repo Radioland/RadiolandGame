c2bdc69316143ff69ad3d9d8ec485c1219f18727
Scott Todd <scott.todd0@gmail.com>
2015-03-31 17:30:52 -0400

    Move fog particles up based on fog level.

--- Game/Assets/_Radioland/Scripts/World/DarkFogControl.cs
+++ Game/Assets/_Radioland/Scripts/World/DarkFogControl.cs
@@ -7,7 +7,13 @@ public class DarkFogControl : MonoBehaviour
 {
     public float fogLevel = 0f;
 
+    [Header("Linked Objects")]
+    [SerializeField] private GameObject groundFogParent;
     [SerializeField] private Slider displaySlider;
+
+    [Header("Configuration")]
+    [SerializeField] private float groundFogMinHeight = 0f;
+    [SerializeField] private float groundFogMaxHeight = 50f;
 
     private float fogLevelTarget;
     private float fogSmoothVel = 0f;
@@ -26,8 +32,14 @@ public class DarkFogControl : MonoBehaviour
 
     private void Update() {
         fogLevel = Mathf.SmoothDamp(fogLevel, fogLevelTarget, ref fogSmoothVel, fogSmoothTime);
+        fogLevel = Mathf.Clamp01(fogLevel);
 
         if (displaySlider) { displaySlider.value = fogLevel; }
+
+        if (groundFogParent) {
+            float y = Mathf.Lerp(groundFogMinHeight, groundFogMaxHeight, fogLevel);
+            groundFogParent.transform.position = new Vector3(0f, y, 0f);
+        }
     }
 
     private void OnChangeFogLevel(float change) {
