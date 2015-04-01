4267a105a54f955412ee032c352dd4f480f1783c
Tim Terrezza <terret@rpi.edu>
2015-04-01 18:38:53 -0400

    disappearing cube (via shrinkage)

---
+++ Game/Assets/_Radioland/Scripts/World/ShrinkCube.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrinkCube : MonoBehaviour {
+	private Vector3 scaleValue;
+	private Vector3 shrinkValue;
+	private Vector3 initialScale;
+	//Random value for speed.  Lower numbers = more speed
+	private float shrinkDivider = 2;
+	private bool shouldShrink = false;
+	private bool shouldGrow = false;
+	private bool disappeared = false;
+	private float disappearedTime;
+	private float growDelay = 5;
+
+	void Start () {
+		initialScale = transform.localScale;
+		scaleValue = transform.localScale;
+		shrinkValue = new Vector3(scaleValue.x / shrinkDivider, scaleValue.y / shrinkDivider, scaleValue.z / shrinkDivider);
+	}
+
+	
+	void Update () {
+		if (shouldShrink) {
+			if (transform.localScale.x >= 0) {
+				scaleValue -= shrinkValue * Time.deltaTime;
+				transform.localScale = scaleValue;
+			}
+			else {
+				shouldShrink = false;
+				GetComponents<BoxCollider>()[0].enabled = false;
+				GetComponents<BoxCollider>()[1].enabled = false;
+				disappearedTime = Time.time;
+				disappeared = true;
+			}
+		}
+		if (Time.time - disappearedTime > growDelay && disappeared) {
+			GetComponents<BoxCollider>()[0].enabled = true;
+			GetComponents<BoxCollider>()[1].enabled = true;
+			shouldGrow = true;
+		}
+		if (shouldGrow) {
+			if (transform.localScale.x <= initialScale.x) {
+				scaleValue += shrinkValue * Time.deltaTime;
+				transform.localScale = scaleValue;
+			}
+			else {
+				disappeared = false;
+				transform.localScale = initialScale;
+				scaleValue = initialScale;
+				shouldGrow = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject.tag == "Player") {
+			shouldShrink = true;
+			shouldGrow = false;
+			disappeared = false;
+		}
+	}
+}
---
+++ Game/Assets/_Radioland/Scripts/World/SpinningCube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningCube : MonoBehaviour {
+	[SerializeField] private float spinDelay;
+	[SerializeField] private float flipDuration = 1f;
+	[SerializeField] private bool oppositeDirection = false;
+	private float rotationTime;
+	private bool rotating;
+	private float rotationDegrees;
+
+	private Quaternion initialRotation;
+
+	void Start () {
+		rotationTime = Time.time;
+		initialRotation = transform.rotation;
+		rotating = true;
+	}
+
+	void Update () {
+		if (rotating) {
+			float t = (Time.time - rotationTime) / flipDuration;
+			float angle = Mathf.Lerp(0f, 90f, t);
+			if (oppositeDirection) { angle *= -1; }
+			transform.rotation = initialRotation * Quaternion.Euler(0f, 0f, angle);
+			if (t > 1) {
+				rotating = false;
+				rotationTime = Time.time;
+			}
+		}
+		else {
+			if (Time.time - rotationTime > spinDelay) {
+				rotating = true;
+				rotationTime = Time.time;
+				initialRotation = transform.rotation;
+			}
+		}
+	}
+}
