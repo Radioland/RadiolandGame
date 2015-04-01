7850f084c9419e8ad9ce4a76d80fd3f963df2c8a
Scott Todd <scott.todd0@gmail.com>
2015-04-01 17:02:21 -0400

    Added wander behavior, cycle between wander and chase.

--- Game/Assets/_Radioland/Scripts/AI/BabyBirbControl.cs
+++ Game/Assets/_Radioland/Scripts/AI/BabyBirbControl.cs
@@ -10,21 +10,44 @@ public class BabyBirbControl : MonoBehaviour
     }
 
     [SerializeField] private BirbState state;
-    private GameObject player;
-    private Transform followTarget;
-    private AIMovement movement;
 
     [Header("Activation")]
-    private const float minActivateSignalStrength = 0.5f;
     [SerializeField] private float minActivateDistance = 10f;
+    private const float minActivateSignalStrength = 0.5f;
 
-    [Header("Chasing")]
+    [Header("General Movement")]
     [SerializeField] private float slowRadius = 2f;
     [SerializeField] private float timeToTargetSpeed = 1f;
 
+    [Header("Wander Movement")]
+    [SerializeField] [Range(0f, 1f)] private float wanderFrequency = 0.5f;
+    [SerializeField] private float wanderToChaseRange = 10f;
+    [SerializeField] private float wanderXZRange = 3f;
+    [SerializeField] private float wanderYMin = -1f;
+    [SerializeField] private float wanderYMax = 5f;
+    private const float reachTargetRadius = 1f;
+
+    private GameObject player;
+    private Transform playerFollowTarget;
+    private AIMovement movement;
+
+    private static GameObject followObjectParent;
+    private Transform followTarget;
+    private float distanceToTarget;
+
     private void Awake() {
+        if (!followObjectParent) {
+            followObjectParent = new GameObject("BabyBirb FollowObjectParent");
+            followObjectParent.transform.position = Vector3.zero;
+        }
+
         player = GameObject.FindWithTag("Player");
-        followTarget = GameObject.FindWithTag("babybirbtarget").transform;
+        playerFollowTarget = GameObject.FindWithTag("babybirbtarget").transform;
+
+        GameObject followObject = new GameObject(name + "_followTarget");
+        followObject.transform.parent = followObjectParent.transform;
+        followTarget = followObject.transform;
+        followTarget.position = transform.position;
 
         movement = gameObject.GetComponent<AIMovement>();
     }
@@ -36,27 +59,86 @@ public class BabyBirbControl : MonoBehaviour
     private void Update() {
         switch (state) {
             case BirbState.Perched:
+                HandlePerched();
                 break;
             case BirbState.Chasing:
-                HandleChase();
+                HandleChasing();
                 break;
             case BirbState.Wander:
+                HandleWander();
                 break;
             default:
                 break;
         }
 
-        transform.LookAt(followTarget);
-    }
+        Vector3 vectorToTarget = followTarget.position - transform.position;
+        distanceToTarget = vectorToTarget.magnitude;
+        float targetSpeed = movement.maxLinearSpeed * (distanceToTarget / slowRadius);
 
-    private void HandleChase() {
-        Vector3 directionToTarget = followTarget.position - transform.position;
-        float distance = directionToTarget.magnitude;
-        float targetSpeed = movement.maxLinearSpeed * (distance / slowRadius);
-
-        Vector3 targetVelocity = directionToTarget.normalized * targetSpeed;
+        Vector3 targetVelocity = vectorToTarget.normalized * targetSpeed;
 
         movement.linearAcceleration = (targetVelocity - movement.linearVelocity) / timeToTargetSpeed;
+
+        if (movement.linearSpeed > 0) {
+            Quaternion rotation = Quaternion.LookRotation(movement.linearVelocity, Vector3.up);
+            transform.rotation = rotation;
+        }
+
+        if (distanceToTarget < reachTargetRadius) {
+            UpdateState();
+        }
+    }
+
+    private void HandlePerched() {
+
+    }
+
+    private void HandleChasing() {
+
+    }
+
+    private void HandleWander() {
+        if (distanceToTarget > wanderToChaseRange) {
+            SetState(BirbState.Chasing);
+        }
+    }
+
+    private void UpdateState() {
+        switch (state) {
+            case BirbState.Perched:
+                break;
+            case BirbState.Chasing:
+                SetState(BirbState.Wander);
+                break;
+            case BirbState.Wander:
+                if (Random.value > wanderFrequency) {
+                    SetState(BirbState.Chasing);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ChooseTarget() {
+        switch (state) {
+            case BirbState.Perched:
+                followTarget.position = transform.position;
+                break;
+            case BirbState.Chasing:
+                followTarget.position = playerFollowTarget.position;
+                break;
+            case BirbState.Wander:
+                float offsetX = Random.Range(-wanderXZRange, wanderXZRange);
+                float offsetY = Random.Range(-wanderYMin, wanderYMax);
+                float offsetZ = Random.Range(-wanderXZRange, wanderXZRange);
+                Vector3 offset = new Vector3(offsetX, offsetY, offsetZ);
+
+                followTarget.position = playerFollowTarget.position + offset;
+                break;
+            default:
+                break;
+        }
     }
 
     public void Activate(float signalStrength) {
@@ -64,8 +146,13 @@ public class BabyBirbControl : MonoBehaviour
             signalStrength > minActivateSignalStrength &&
             Vector3.Distance(transform.position, player.transform.position) < minActivateDistance) {
 
-            state = BirbState.Chasing;
+            SetState(BirbState.Chasing);
         }
+    }
+
+    private void SetState(BirbState newState) {
+        state = newState;
+        ChooseTarget();
     }
 
     public void OnDrawGizmos() {
@@ -77,6 +164,6 @@ public class BabyBirbControl : MonoBehaviour
         if (!Application.isPlaying) { return; }
 
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(followTarget.position, slowRadius);
+        Gizmos.DrawWireSphere(playerFollowTarget.position, slowRadius);
     }
 }
