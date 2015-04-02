58df0a0502ccbad5b40244337404d231c753ff7c
Scott Todd <scott.todd0@gmail.com>
2015-04-01 23:13:17 -0400

    Added ignoreObstacles setting to CurveWalker, pause on (faked) collision.

--- Game/Assets/_Radioland/Scripts/Curves/CurveWalker.cs
+++ Game/Assets/_Radioland/Scripts/Curves/CurveWalker.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public enum CurveWalkerMode
 {
@@ -11,20 +12,31 @@ public enum CurveWalkerMode
 public class CurveWalker : MonoBehaviour
 {
     [SerializeField] private ICurve curve;
+
+    [Header("Basic Settings")]
     [SerializeField] private float duration = 2f;
     [SerializeField] private float pauseTime = 0f;
     [SerializeField] private Interpolate.EaseType easeType = Interpolate.EaseType.EaseInOutQuad;
     [SerializeField] private bool lookForward = false;
     [SerializeField] private CurveWalkerMode mode = CurveWalkerMode.PingPong;
 
+    [Header("Obstacle Settings")]
+    [SerializeField] private bool ignoreObstacles = true;
+
     private float progress = 0f;
+    private float easedProgress = 0f;
     private float timePaused = 0f;
     private bool goingForward = true;
     private bool paused = false;
     private Interpolate.Function easeFunction;
 
+    private Collider myCollider;
+    private Vector3 checkPosition;
+
     private void Awake() {
         easeFunction = Interpolate.Ease(easeType);
+
+        myCollider = gameObject.GetComponentInChildren<Collider>();
     }
 
     private void Start() {
@@ -40,6 +52,19 @@ public class CurveWalker : MonoBehaviour
             } else {
                 return;
             }
+        }
+
+        if (!ignoreObstacles) {
+            int orientation = goingForward ? 1 : -1;
+            float forward = myCollider ? myCollider.bounds.extents.x : 0f;
+            Vector3 direction = curve.GetDirection(easedProgress);
+            checkPosition = transform.position + direction * forward * orientation;
+
+            Collider[] hitColliders = Physics.OverlapSphere(checkPosition, 0.3f);
+
+            Debug.DrawRay(transform.position, direction, Color.magenta);
+
+            if (hitColliders.Any(hitCollider => hitCollider != myCollider)) { return; }
         }
 
         if (goingForward) {
@@ -64,12 +89,19 @@ public class CurveWalker : MonoBehaviour
             }
         }
 
-        float easedProgress = easeFunction(0f, 1f, progress, 1f);
+        easedProgress = easeFunction(0f, 1f, progress, 1f);
 
         Vector3 position = curve.GetPoint(easedProgress);
         transform.position = position;
         if (lookForward) {
             transform.LookAt(position + curve.GetDirection(easedProgress));
         }
+    }
+
+    public void OnDrawGizmos() {
+        if (ignoreObstacles) { return; }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(checkPosition, 1f);
     }
 }
