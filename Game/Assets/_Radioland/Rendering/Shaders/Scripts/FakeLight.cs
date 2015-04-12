a4b2665b269d371817c10c289c1f56917e0b0875
Xinoph <b.tam@live.com>
2015-04-12 16:26:12 -0400

    Adjusted baby birb material. Fixed light culling for eyes.

--- Game/Assets/_Radioland/Rendering/Shaders/Scripts/FakeLight.cs
+++ Game/Assets/_Radioland/Rendering/Shaders/Scripts/FakeLight.cs
@@ -7,14 +7,27 @@ public class FakeLight : MonoBehaviour {
 	public Color lit;
 	public Color shade;
 
+	private Color m_lit;
+	private Color m_shade;
+
 	// Use this for initialization
 	void Start () {
 		GetComponent<Renderer>().material.SetColor("_Lit", lit);
+		m_lit = lit;
 		GetComponent<Renderer>().material.SetColor("_Shade", shade);
+		m_shade = shade;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (m_lit != lit) {
+			GetComponent<Renderer>().material.SetColor("_Lit", lit);
+			m_lit = lit;
+		}
+		if (m_shade != shade) {
+			GetComponent<Renderer>().material.SetColor("_Shade", shade);
+			m_shade = shade;
+		}
 		GetComponent<Renderer>().material.SetVector("_LightSource", lightSource.transform.position);
 	}
 }
