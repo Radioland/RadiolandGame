24ced73d79f3ee7ed75cac799190c4c719dcf8f1
Xinoph <b.tam@live.com>
2015-04-13 15:17:35 -0400

    Added UI collectibles prototype.

---
+++ Game/Assets/_Radioland/UI-collectables/uiCollectableIcon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uiCollectableIcon : MonoBehaviour {
+
+	public bool isUnlocked;
+
+	public Material unlockedMaterial;
+
+	private bool materialSet;
+
+    private Vector3 rotateDegreesPerSecond;
+    // public bool ignoreTimescale;
+    private float m_LastRealTime;
+
+    private Quaternion initialRotation;
+
+
+    private void Start()
+    {
+        m_LastRealTime = Time.realtimeSinceStartup;
+        initialRotation = transform.rotation;
+        materialSet = false;
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        // if (ignoreTimescale)
+        // {
+            deltaTime = (Time.realtimeSinceStartup - m_LastRealTime);
+            m_LastRealTime = Time.realtimeSinceStartup;
+        // }
+        transform.Rotate(rotateDegreesPerSecond*deltaTime);
+
+        if (isUnlocked && !materialSet) {
+        	GetComponent<Renderer>().material = unlockedMaterial;
+        	materialSet = true;
+        }
+    }
+
+    public void startRotate() {
+    	rotateDegreesPerSecond.y = 30f;
+    }
+
+    public void stopRotate() {
+    	rotateDegreesPerSecond.y = 0;
+    	transform.rotation = initialRotation;
+    }
+
+
+}
