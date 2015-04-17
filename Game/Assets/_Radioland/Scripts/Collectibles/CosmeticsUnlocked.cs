c7e23950f19ec6c01d0cd6f6c5df7faf4d4ef5e1
Scott Todd <scott.todd0@gmail.com>
2015-04-17 10:36:58 -0400

    Started on cosmetics unlocked script, refactoring soon.

---
+++ Game/Assets/_Radioland/Scripts/Collectibles/CosmeticsUnlocked.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static internal class CosmeticsUnlockedInitializer
+{
+    //Disable the unused variable warning
+#pragma warning disable 0414
+    static private CosmeticsUnlocked cosmeticsUnlocked = (new GameObject("CosmeticsUnlocked")).AddComponent<CosmeticsUnlocked>();
+#pragma warning restore 0414
+
+    static public void Unlock(string collectibleName) {
+        switch (collectibleName) {
+            case "flowerhat":
+                cosmeticsUnlocked.flowerHatState = CosmeticsUnlocked.UnlockState.Unlocked;
+                break;
+            case "mustache":
+                cosmeticsUnlocked.mustacheState = CosmeticsUnlocked.UnlockState.Unlocked;
+                break;
+            case "birbhat":
+                cosmeticsUnlocked.birbHatState = CosmeticsUnlocked.UnlockState.Unlocked;
+                break;
+            default:
+                Debug.Log("Unknown collectible name:" + collectibleName);
+                break;
+        }
+    }
+}
+
+public class CosmeticsUnlocked : MonoBehaviour
+{
+    public enum UnlockState
+    {
+        Locked, Unlocked, Equipped
+    }
+
+    public UnlockState flowerHatState = UnlockState.Locked;
+    public UnlockState mustacheState = UnlockState.Locked;
+    public UnlockState birbHatState = UnlockState.Locked;
+
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+}
--- Game/Assets/_Radioland/UI-collectables/uiCollectableIcon.cs
+++ Game/Assets/_Radioland/UI-collectables/uiCollectableIcon.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class uiCollectableIcon : MonoBehaviour {
-
-	public bool isUnlocked;
-
-	public Material unlockedMaterial;
-
-	private bool materialSet;
+public class uiCollectableIcon : MonoBehaviour
+{
+    public bool isUnlocked;
+    public Material unlockedMaterial;
+    private bool materialSet;
 
     private Vector3 rotateDegreesPerSecond;
     // public bool ignoreTimescale;
@@ -15,14 +13,12 @@ public class uiCollectableIcon : MonoBehaviour {
 
     private Quaternion initialRotation;
 
-
     private void Start()
     {
         m_LastRealTime = Time.realtimeSinceStartup;
         initialRotation = transform.rotation;
         materialSet = false;
     }
-
 
     // Update is called once per frame
     private void Update()
@@ -33,22 +29,20 @@ public class uiCollectableIcon : MonoBehaviour {
             deltaTime = (Time.realtimeSinceStartup - m_LastRealTime);
             m_LastRealTime = Time.realtimeSinceStartup;
         // }
-        transform.Rotate(rotateDegreesPerSecond*deltaTime);
+        transform.Rotate(rotateDegreesPerSecond * deltaTime);
 
         if (isUnlocked && !materialSet) {
-        	GetComponent<Renderer>().material = unlockedMaterial;
-        	materialSet = true;
+            GetComponent<Renderer>().material = unlockedMaterial;
+            materialSet = true;
         }
     }
 
     public void startRotate() {
-    	rotateDegreesPerSecond.y = 30f;
+        rotateDegreesPerSecond.y = 30f;
     }
 
     public void stopRotate() {
-    	rotateDegreesPerSecond.y = 0;
-    	transform.rotation = initialRotation;
+        rotateDegreesPerSecond.y = 0;
+        transform.rotation = initialRotation;
     }
-
-
 }
