4f36ad9a5800d8835a1566afe94d14ca5ce5e508
Scott Todd <scott.todd0@gmail.com>
2015-04-20 11:34:09 -0400

    Started building UI and laying out scripts.

---
+++ Game/Assets/_Radioland/Scripts/DialogueManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueUI;
+    [SerializeField] private Text justText;
+    // TODO: Text + Image layout
+
+    // TODO: adjust camera look target when in dialogue (y from 2.5 to 0.5-1?)
+
+    private Dialogue currentDialogue;
+
+    private void Awake() {
+
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+
+    public bool CanStartDialogue() {
+        return currentDialogue == null;
+    }
+
+    public void StartDialogue(Dialogue nextDialogue) {
+        if (!CanStartDialogue()) {
+            Debug.LogWarning("Tried to start dialogue when not able to.");
+        }
+
+        currentDialogue = nextDialogue;
+    }
+
+    public void SetMessage(string messageText) {
+        dialogueUI.SetActive(true);
+
+        justText.text = messageText;
+        justText.enabled = true;
+    }
+}
