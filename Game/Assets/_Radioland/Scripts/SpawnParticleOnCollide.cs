8cf07ae0e722abfb8efe020ca88f1328cc32e3c1
Xinoph <b.tam@live.com>
2015-04-26 22:07:14 -0400

    Trying new star particle system...

---
+++ Game/Assets/_Radioland/Scripts/SpawnParticleOnCollide.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnParticleOnCollide : MonoBehaviour {
+	public ParticleSystem thisParticleSystem;
+    public ParticleCollisionEvent[] collisionEvents;
+
+    public ParticleSystem subParticleSystem;
+    [Range(0,1)]
+    public float spawnChance;
+    
+    void Start() {
+        thisParticleSystem = GetComponent<ParticleSystem>();
+        collisionEvents = new ParticleCollisionEvent[16];
+    }
+
+    void OnParticleCollision(GameObject other) {
+        int safeLength = thisParticleSystem.GetSafeCollisionEventSize();
+        if (collisionEvents.Length < safeLength)
+            collisionEvents = new ParticleCollisionEvent[safeLength];
+        
+        int numCollisionEvents = thisParticleSystem.GetCollisionEvents(other, collisionEvents);
+        int i = 0;
+        //while (i < numCollisionEvents) {
+            if (Random.value <= spawnChance) {
+                Vector3 pos = collisionEvents[i].intersection;
+                GameObject subParticle = (GameObject)Instantiate(subParticleSystem, pos, subParticleSystem.transform.rotation);
+            	subParticle.transform.SetParent(thisParticleSystem.transform);
+
+            	// Want to destroy this specific particle that collided...
+            }
+           // i++;
+        //}
+    }
+}
---
+++ Game/Assets/_Radioland/Scripts/World/RandomRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	[Tooltip("Euler angle degrees range, negative to positive.")]
+	[SerializeField] private Vector3 angularRange;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		float x = Random.Range(-angularRange.x, angularRange.x);
+		float y = Random.Range(-angularRange.y, angularRange.y);
+		float z = Random.Range(-angularRange.z, angularRange.z);
+		transform.Rotate(new Vector3(x,y,z));
+	}
+}
