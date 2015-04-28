71f19147b4b3c87229cd18266711a6493bf4187c
Xinoph <b.tam@live.com>
2015-04-27 21:28:52 -0400

    Added outfit-butterfly. Added tape to nerd glasses.

---
+++ Game/Assets/_Radioland/Scripts/Util/PingPongRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongRotate : MonoBehaviour
+{
+    [SerializeField] private Vector3 minRotation;
+    [SerializeField] private Vector3 maxRotation;
+    [SerializeField] private float speed = 1.0f;
+
+    private void Awake() {
+
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        float pingPong = Mathf.PingPong(Time.time * speed, 1.0f);
+        Quaternion thisRotation = Quaternion.Slerp(Quaternion.Euler(minRotation), Quaternion.Euler(maxRotation), pingPong);
+        transform.localRotation = thisRotation;
+    }
+}
