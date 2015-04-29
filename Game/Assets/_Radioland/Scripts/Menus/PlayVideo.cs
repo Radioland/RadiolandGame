032e12663e21f81de224c830d16002109baf8322
Scott Todd <scott.todd0@gmail.com>
2015-04-28 20:42:23 -0400

    Rescale to fit fullscreen while keeping aspect ratio.

--- Game/Assets/_Radioland/Scripts/Menus/PlayVideo.cs
+++ Game/Assets/_Radioland/Scripts/Menus/PlayVideo.cs
@@ -10,6 +10,7 @@ public class PlayVideo : MonoBehaviour
 
     private MovieTexture movie;
     private AudioSource source;
+    private Camera mainCamera;
 
     private const float padding = 1f;
 
@@ -18,6 +19,30 @@ public class PlayVideo : MonoBehaviour
         movie = (MovieTexture) myRenderer.material.mainTexture;
 
         source = gameObject.GetComponent<AudioSource>();
+
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+
+        float movieAspect = movie.width * 1.0f / movie.height;
+        float cameraAspect = mainCamera.aspect;
+
+        Vector2 cameraTopLeft = mainCamera.ViewportToWorldPoint(new Vector3(0f, 1f, mainCamera.nearClipPlane));
+        Vector2 cameraBottomRight = mainCamera.ViewportToWorldPoint(new Vector3(1f, 0f, mainCamera.nearClipPlane));
+        float worldSpaceWidth = cameraBottomRight.x - cameraTopLeft.x;
+        float worldSpaceHeight = cameraTopLeft.y - cameraBottomRight.y;
+
+        // Rescale to fit fullscreen while keeping aspect ratio.
+        float width, height;
+        if (movieAspect > cameraAspect) {
+            // Movie wider than screen, space at top and bottom (not full height).
+            width = worldSpaceWidth;
+            height = worldSpaceWidth / movieAspect;
+        } else {
+            // Movie taller than screen, space at left and right (not full width).
+            height = worldSpaceHeight;
+            width = worldSpaceHeight * movieAspect;
+        }
+        // Transform pivot is centered in X and Y, set scale.
+        transform.localScale = new Vector3(width, height, 1f);
 
         movie.Play();
         source.Play();
