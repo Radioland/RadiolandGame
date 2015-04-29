af8b6c63d2b005d7a79effca439b0ea489746c40
Scott Todd <scott.todd0@gmail.com>
2015-04-28 20:27:47 -0400

    Imported videos, setup scene that plays a video until finished.

---
+++ Game/Assets/_Radioland/Scripts/Menus/PlayVideo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(AudioSource))]
+public class PlayVideo : MonoBehaviour
+{
+    [SerializeField] private SimpleMenu menu;
+    [SerializeField] private float duration;
+
+    private MovieTexture movie;
+    private AudioSource source;
+
+    private const float padding = 1f;
+
+    private void Awake() {
+        Renderer myRenderer = gameObject.GetComponent<Renderer>();
+        movie = (MovieTexture) myRenderer.material.mainTexture;
+
+        source = gameObject.GetComponent<AudioSource>();
+
+        movie.Play();
+        source.Play();
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+        if (Time.time > duration + padding || Input.GetButtonDown("Submit")) {
+            menu.LoadNextLevel();
+        }
+    }
+}
