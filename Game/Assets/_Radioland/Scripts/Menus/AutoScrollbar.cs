45af26ab346b7e3d8d216f0721f9f619f364eb2d
Xinoph <b.tam@live.com>
2015-04-30 18:26:24 -0400

    Scrolling credits text on end screen :)

---
+++ Game/Assets/_Radioland/Scripts/Menus/AutoScrollbar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AutoScrollbar : MonoBehaviour {
+
+	public Scrollbar m_scrollbar;
+
+	public float time = 1f;
+	private float m_time;
+
+	// Use this for initialization
+	void Start () {
+		m_time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_scrollbar.value = Mathf.Lerp(1, 0, Mathf.Repeat(m_time + Time.time / time, 1));
+	}
+}
