55dd916827d4b15916fa79c95bd4d34e9437d53f
Scott Todd <scott.todd0@gmail.com>
2015-05-01 14:38:15 -0400

    Started setting up scripts for level select, added screenshots.

---
+++ Game/Assets/_Radioland/Scripts/Menus/LevelSelectController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectController : MonoBehaviour
+{
+    private LevelSelectEntry[] levelSelectEntries;
+
+    private void Awake() {
+        levelSelectEntries = gameObject.GetComponentsInChildren<LevelSelectEntry>();
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+}
---
+++ Game/Assets/_Radioland/Scripts/Menus/LevelSelectEntry.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelSelectEntry : MonoBehaviour
+{
+    [SerializeField] private Sprite largePreview;
+    [SerializeField] private string levelName;
+    [SerializeField] private string levelDescription;
+
+    private void Reset() {
+        Image image = gameObject.GetComponent<Image>();
+        if (image) {
+            largePreview = image.sprite;
+        }
+    }
+
+    private void Awake() {
+        Text childText = gameObject.GetComponentInChildren<Text>();
+        if (childText) {
+            childText.text = levelName;
+        }
+    }
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+}
