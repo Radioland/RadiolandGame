66164b415a441dbe654e5dfbe24e7bbadf3e1cc2
Scott Todd <scott.todd0@gmail.com>
2015-05-01 16:17:02 -0400

    Radio controls selection, disabled navigation on buttons.

--- Game/Assets/_Radioland/Scripts/Menus/LevelSelectController.cs
+++ Game/Assets/_Radioland/Scripts/Menus/LevelSelectController.cs
@@ -5,38 +5,48 @@ using UnityEngine.EventSystems;
 
 public class LevelSelectController : MonoBehaviour
 {
+    [Header("Scene Objects")]
     [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private RadioControl radioControl;
+    [SerializeField] private GameObject levelSelectEntriesParent;
+    [SerializeField] private SimpleMenu simpleMenu;
+    [Header("Display Objects")]
     [SerializeField] private Text titleText;
     [SerializeField] private Text descriptionText;
     [SerializeField] private Image largePreviewImage;
 
-    private GameObject lastSelectedGameObject;
+    private LevelSelectEntry[] levelSelectEntries;
+    private int lastStrongestSignalId;
 
     private void Awake() {
-
+        levelSelectEntries = levelSelectEntriesParent.GetComponentsInChildren<LevelSelectEntry>();
     }
 
     private void Start() {
-        lastSelectedGameObject = eventSystem.currentSelectedGameObject;
+        lastStrongestSignalId = radioControl.strongestSignalStation.id;
 
         UpdateDisplay();
     }
 
     private void Update() {
-        if (lastSelectedGameObject != eventSystem.currentSelectedGameObject) {
-            lastSelectedGameObject = eventSystem.currentSelectedGameObject;
+        if (!radioControl.strongestSignalStation.StrongSignal()) {
+            return;
+        }
 
+        if (lastStrongestSignalId != radioControl.strongestSignalStation.id) {
+            lastStrongestSignalId = radioControl.strongestSignalStation.id;
             UpdateDisplay();
         }
     }
 
-    private void UpdateDisplay() {
-        if (!lastSelectedGameObject) { return; }
-        LevelSelectEntry entry = lastSelectedGameObject.GetComponent<LevelSelectEntry>();
+    private void CheckRadio() {
+        lastStrongestSignalId = radioControl.strongestSignalStation.id;
+    }
 
-        if (!entry) {
-            return;
-        }
+    private void UpdateDisplay() {
+        LevelSelectEntry entry = levelSelectEntries[lastStrongestSignalId-1];
+
+        eventSystem.SetSelectedGameObject(entry.gameObject);
 
         largePreviewImage.sprite = entry.largePreview;
         titleText.text = entry.levelName;
